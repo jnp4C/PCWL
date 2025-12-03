065fac064176d1082cf54dde5cef14d915c22c14
jnp4C <jonypony4@gmail.com>
2025-12-03 21:52:32 +0100

    Update party leaderboard rules and chat send button

---
+++ backend/game/migrations/0027_merge_20251202_0001.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("game", "0026_auto_20251201_1833"),
+        ("game", "0026_districtddosentry_ended_by_player"),
+    ]
+
+    operations = []
