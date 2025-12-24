0f97f27f47b0c257ee9ea800cf8fd140c8710299
jnp4C <jonypony4@gmail.com>
2025-12-24 13:19:16 +0100

    Merge 0029 migrations

---
+++ backend/game/migrations/0030_merge_0029.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("game", "0029_district_chat_votes"),
+        ("game", "0029_districtfirewallentry"),
+    ]
+
+    operations = []
